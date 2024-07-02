1f84d462a2918a8d746a2e917a968e963912b891
edersonff <edersonff_@hotmail.com.br>
2024-07-02 22:23:53 +0000

    fix: added real projects

--- src/components/Project/projects.ts
+++ src/components/Project/projects.ts
@@ -11,42 +11,42 @@ export type Project = {
 
 export const projects: Project[] = [
   {
-    title: "Desenvolvimento Site <color>Missner</color>",
+    title: "Desenvolvimento Plataforma <color>Zap Auto</color>",
     description:
-      "Site institucional para indústria 100% responsivo e 100% administravel + blog de notícias.",
+      "Software para automação de mensagens no WhatsApp, utilizando modelo de IA para responder mensagens automaticamente.",
     image: "https://cobra.art.br/wp-content/uploads/2023/04/site-missner.jpg",
     color: "#85C6E4",
-    scripts: ["html", "css", "javascript", "tailwind", "typescript"],
+    scripts: ["tailwind", "typescript", "nextjs", "postgresql", "cloud"],
     url: "https://missner.com.br",
   },
   {
-    title: "Identidade Visual <color>SFICHIPS</color>",
+    title: "Criação do Software <color>Alarc Tools</color>",
     description:
-      "Site institucional para indústria 100% responsivo e 100% administravel + blog de notícias.",
+      "Plataforma Web criada para gerenciar e automatizar levantamento de preço de entrega de produtos do Mercado Livre por parte de vendedores.",
     image:
       "https://cobra.art.br/wp-content/uploads/2023/04/nova-id-sfichips-1024x1024.jpg",
     color: "#B10002",
-    scripts: ["html", "css", "javascript", "tailwind", "typescript"],
+    scripts: ["tailwind", "typescript", "nextjs", "postgresql", "heroku"],
     url: "https://sfichips.com.br",
   },
   {
-    title: "Branding Para <color>Buenos Barber</color>",
+    title: "Homepage <color>Merlin Tech</color>",
     description:
-      "Site institucional para indústria 100% responsivo e 100% administravel + blog de notícias.",
+      "Site institucional para o setor de tecnologia da empresa Merlin, utilizado para receber contatos por email.",
     image:
       "https://cobra.art.br/wp-content/uploads/2023/04/logo-buenos-1024x1024.jpg",
     color: "#FFF8DB",
-    scripts: ["html", "css", "javascript", "tailwind", "typescript"],
+    scripts: ["tailwind", "typescript", "nextjs", "vercel"],
     url: "https://buenosbarber.com.br",
   },
   {
-    title: "Desenvolvimento De <color>Embalagem Aminna</color>",
+    title: "Homepage da <color>Alarc</color>",
     description:
       "Site institucional para indústria 100% responsivo e 100% administravel + blog de notícias.",
     image:
       "https://cobra.art.br/wp-content/uploads/2023/04/embalagem-cookies-aminna-1024x1024.jpg",
     color: "#FFB901",
-    scripts: ["html", "css", "javascript", "tailwind", "typescript"],
+    scripts: ["tailwind", "typescript", "nextjs", "vercel"],
     url: "https://aminna.com.br",
   },
 ];
