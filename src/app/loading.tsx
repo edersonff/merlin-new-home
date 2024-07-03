bb71100bc3feb342554a31617d32283bd2d87d9a
edersonff <edersonff_@hotmail.com.br>
2024-07-03 13:45:10 -0300

    feat: loading screen created

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,10 +1,44 @@
-import Image from "next/image";
-import React from "react";
+"use client";
 
-export default function Loading() {
+import { motion, AnimatePresence } from "framer-motion";
+import Image from "next/image";
+import React, { useEffect } from "react";
+
+export default function Loading({ isComponent }: { isComponent: boolean }) {
   return (
-    <div className="fixed w-full h-full flex-center">
-      <Image src="/merlin/logo.svg" alt="Loading Logo" width={30} height={30} />
-    </div>
+    <motion.div
+      className="fixed-full z-[999] flex-center small:px-6 bg-gradient-to-br from-primary to-violet-500 overflow-hidden"
+      animate={{ height: "100vh" }}
+      exit={{ height: "0vh" }}
+      transition={{
+        duration: 1,
+        delay: 1,
+        type: "tween",
+      }}
+    >
+      <motion.div
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{
+          duration: 1,
+          delay: 0.5,
+          type: "tween",
+        }}
+        className="mx-auto  rounded-xl drop-shadow-xl relative"
+      >
+        <div className="p-3 animate-spin drop-shadow-2xl bg-gradient-to-bl from-purple-400 via-primary to-indigo-600 md:w-48 md:h-48 h-32 w-32 aspect-square rounded-full">
+          <div className="rounded-full h-full w-full bg-slate-100 dark:bg-zinc-900 background-blur-md"></div>
+        </div>
+        <div className="absolute-full flex-center">
+          <Image
+            src="/merlin/logo-white.svg"
+            alt="Logo"
+            loading="eager"
+            width={40}
+            height={40}
+          />
+        </div>
+      </motion.div>
+    </motion.div>
   );
 }
