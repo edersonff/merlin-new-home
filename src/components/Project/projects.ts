587c1967be8de7e3da34902ca5b652547fc49d7d
edersonff <edersonff_@hotmail.com.br>
2024-07-03 13:46:08 -0300

    fix: projects images, colors and urls

--- src/components/Project/projects.ts
+++ src/components/Project/projects.ts
@@ -14,39 +14,39 @@ export const projects: Project[] = [
     title: "Desenvolvimento Plataforma <color>Zap Auto</color>",
     description:
       "Software para automação de mensagens no WhatsApp, utilizando modelo de IA para responder mensagens automaticamente.",
-    image: "https://cobra.art.br/wp-content/uploads/2023/04/site-missner.jpg",
-    color: "#85C6E4",
+      image:
+        "/img/mockups/04.webp",
+      color: "#05D658",
     scripts: ["tailwind", "typescript", "nextjs", "postgresql", "cloud"],
-    url: "https://missner.com.br",
+    url: "https://zapauto.com.br",
   },
   {
     title: "Criação do Software <color>Alarc Tools</color>",
     description:
       "Plataforma Web criada para gerenciar e automatizar levantamento de preço de entrega de produtos do Mercado Livre por parte de vendedores.",
     image:
-      "https://cobra.art.br/wp-content/uploads/2023/04/nova-id-sfichips-1024x1024.jpg",
-    color: "#B10002",
+      "/img/mockups/03.webp",
+    color: "#0D7CDE",
     scripts: ["tailwind", "typescript", "nextjs", "postgresql", "heroku"],
-    url: "https://sfichips.com.br",
+    url: "https://tools.alarc.com.br",
   },
   {
     title: "Homepage <color>Merlin Tech</color>",
     description:
       "Site institucional para o setor de tecnologia da empresa Merlin, utilizado para receber contatos por email.",
-    image:
-      "https://cobra.art.br/wp-content/uploads/2023/04/logo-buenos-1024x1024.jpg",
-    color: "#FFF8DB",
+      image:
+        "/img/mockups/02.webp",
+      color: "#47236C",
     scripts: ["tailwind", "typescript", "nextjs", "vercel"],
-    url: "https://buenosbarber.com.br",
+    url: "https://tech.merlin.app.br",
   },
   {
     title: "Homepage da <color>Alarc</color>",
     description:
-      "Site institucional para indústria 100% responsivo e 100% administravel + blog de notícias.",
-    image:
-      "https://cobra.art.br/wp-content/uploads/2023/04/embalagem-cookies-aminna-1024x1024.jpg",
-    color: "#FFB901",
+      "Site institucional da empresa Alarc, utilizado internamente para apresentar os serviços da empresa. Utiliza um sistema de blog para postar novidades e artigos.",
+    image: "/img/mockups/01.webp",
+    color: "#94CB5E",
     scripts: ["tailwind", "typescript", "nextjs", "vercel"],
-    url: "https://aminna.com.br",
+    url: "https://alarc.com.br",
   },
 ];
